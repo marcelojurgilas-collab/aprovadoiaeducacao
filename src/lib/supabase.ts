c96371a1a40f75036c38c8fef67245ab948eb206
marcelojurgilas-collab <marcelojurgilas@gmail.com>
2026-04-26 18:11:40 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://iyorylspakpvtfgdpiai.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_nNgQyx3aBKuPn7jldchnwg_xl0hroa5";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5b3J5bHNwYWtwdnRmZ2RwaWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMzM3MzQsImV4cCI6MjA5MjgwOTczNH0.tt_3vGm_jypOqh2rDYWurrbMkYEZC9pkOn_VwiTySCI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
